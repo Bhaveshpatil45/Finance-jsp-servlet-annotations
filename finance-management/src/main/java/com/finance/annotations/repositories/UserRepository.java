8bb9e3dc7b8201e4c04eca44f2919305f335c711
Bhaveshpatil45 <bp700259@gmail.com>
2025-01-28 13:39:09 +0530

    jsp-servlet-annotations

--- finance-management/src/main/java/com/finance/annotations/repositories/UserRepository.java
+++ finance-management/src/main/java/com/finance/annotations/repositories/UserRepository.java
@@ -1,4 +1,22 @@
 package com.finance.annotations.repositories;
 
+import com.finance.annotations.models.User;
+
+import java.util.HashMap;
+import java.util.Optional;
+
 public class UserRepository {
+    private HashMap<String, User> userDatabase = new HashMap<>();
+
+    public void save(User user) {
+        userDatabase.put(user.getUsername(), user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return Optional.ofNullable(userDatabase.get(username));
+    }
+
+    public void update(User user) {
+        userDatabase.put(user.getUsername(), user);
+    }
 }
